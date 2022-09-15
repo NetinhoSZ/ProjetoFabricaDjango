49bda2450e314fc5bc8606213340684af49a7872
NetinhoSZ <netinhoskennedy@gmail.com>
2022-09-15 17:10:03 -0300

    alteração

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def alecrim(request):
+    
+    return render(request, template_name= 'blog/home.html')
+    
+def dourado(request):
+    html = "<html><body><h1>Dourado!</h1></body></html>"
+    return HttpResponse(html)
--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import alecrim, dourado
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('alecrim/', alecrim),
+    path('dourado/', dourado),
 ]
