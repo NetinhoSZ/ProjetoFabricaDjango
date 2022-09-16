691e0b52caf533e059338a55807633fa5559a62c
NetinhoSZ <netinhoskennedy@gmail.com>
2022-09-16 20:56:07 -0300

    finalworkshop

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Postagem
+admin.site.register(Postagem)
--- projeto/urls.py
+++ projeto/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import alecrim, dourado
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alecrim/', alecrim),
-    path('dourado/', dourado),
+    path('', include('blog.urls'))
+    
 ]
